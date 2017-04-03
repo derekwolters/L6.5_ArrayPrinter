b81a487acad6a3b4a6b8ede9fc14359ee43b6b63
Derek Wolters <derek.wolters@gmail.com>
2017-04-03 13:37:15 -0400

    initial commit

---
+++ L6.5_ArrayPrinter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6._5_ArrayPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+       
+    }
+}
